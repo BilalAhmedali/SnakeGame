96b66663d86691e3f451e6e71d015944f804bc96
roll94@yahoo.com <roll94@yahoo.com>
2019-05-08 15:03:05 +0500

    creating quite method

--- snake.py
+++ snake.py
@@ -9,7 +9,21 @@ pygame.display.set_caption("Snake-Game")
 gameExit = False
 gameOver = False
 
+#game features
+
+#Quit game method
+def quiteGame(game_event):
+    if game_event.type == pygame.QUIT:
+        gameExit = True
+
+
 #creating game loop
 while not gameExit:
-    pass
+    for events in pygame.event.get():
+        quiteGame(events)
     
+pygame.quit()
+quit()
+
+
++923415234310
