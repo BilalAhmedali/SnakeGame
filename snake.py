4b5f51f21f3275353fc90325dd21b82cb870e720
roll94@yahoo.com <roll94@yahoo.com>
2019-05-08 17:06:01 +0500

    created snake

--- snake.py
+++ snake.py
@@ -2,28 +2,50 @@ import pygame
 x = pygame.init()
 
 #Initializing Game window
-gameWindow = pygame.display.set_mode((1200,700))
+width = 768
+height = 600
+gameWindow = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Snake-Game")
+update_game  = pygame.display.update
+draw_snake = pygame.draw.rect
+
+#color
+white = (255,255,255)
+black  = (0,0,0)
+red = (255,0,0)
 
 #Specifying game variables
 gameExit = False
 gameOver = False
-
-#game features
-
-#Quit game method
-def quiteGame(game_event):
-    if game_event.type == pygame.QUIT:
-        gameExit = True
+snake_x = 45
+snake_y = 45
+snake_size = 15
+clock = pygame.time.clock()
+fps = 30
 
 
 #creating game loop
 while not gameExit:
     for events in pygame.event.get():
-        quiteGame(events)
+        
+        # creating Quite Event
+        if events.type == pygame.QUIT:
+                gameExit = True
+        
+        #updating-Window color 
+        gameWindow.fill(white)
+        update_game()
+
+        #drawing Snake
+        draw_snake(gameWindow,black,[snake_x,snake_y,snake_size,snake_size])
+        update_game()
+        clock.tick(fps)
+        
+        #moving snake right position
+        
+        
     
 pygame.quit()
 quit()
 
 
-+923415234310
